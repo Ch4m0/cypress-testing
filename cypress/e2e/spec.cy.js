6d75b526ceb72cb78cc8118692f1111f8a3ab3df
Abner Antonio Chamorro <achamorro@pmac-achamorro.local>
2024-02-25 13:27:10 -0500

    login page

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,10 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { LoginMethods } from "./pages/login/login.methods";
+
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://www.demoblaze.com/");
+    cy.get('a[data-target="#logInModal"]').click();
+    LoginMethods.login("hola", "hola");
+    cy.wait(5000);
+  });
+});
