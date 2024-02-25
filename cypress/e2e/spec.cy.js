c8423ea6ef5c871068ea965b4d3b62e8c7fd2640
Abner Antonio Chamorro <achamorro@pmac-achamorro.local>
2024-02-25 14:42:50 -0500

    Creacion de clases para signup:

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,13 +1,22 @@
 import { LoginMethods } from "./pages/login/login.methods";
+import { SignupData } from "./pages/signUp/signUp.data";
+import { SignupMethods } from "./pages/signUp/signup.methods";
 
 describe("template spec", () => {
-  it("passes", () => {
+  it("login", () => {
     const username = "botija";
     const password = "123456";
     cy.visit("https://www.demoblaze.com/");
     cy.get('a[data-target="#logInModal"]').click();
     LoginMethods.login(username, password);
-
     cy.get("a#nameofuser").contains("Welcome botija");
+  });
+
+  it("Signup", () => {
+    const username = "newUser1";
+    const password = "esteesmipassword";
+    cy.visit("https://www.demoblaze.com/");
+    cy.get('a[data-target="#signInModal"]').click();
+    SignupMethods.signup(username, password);
   });
 });
